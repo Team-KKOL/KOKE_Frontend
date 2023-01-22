97ddce21d6ec2df00259eff06b785ea04dec3f4f
JE <wjd7dms7@naver.com>
2023-01-23 03:51:28 +0900

    theme.js 추가 / header 로그인 버전 추가

---
+++ src/theme.js
@@ -0,0 +1,17 @@
+// color 정의
+const colors = {
+  bg: "#fffef8",
+  gray: "#707070",
+  brown: "#b4712f",
+  darkBrown: "#60370f",
+  lightBrown: "#ebe6d4",
+  Beige: "#faf7e8",
+  black: "#222222",
+};
+
+// theme 객체에 감싸서 반환한다.
+const theme = {
+  colors,
+};
+
+export default theme;
