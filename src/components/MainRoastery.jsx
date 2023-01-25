7a58cf3012b6f3b1789a76e08d09e5b8b50af767
JE <wjd7dms7@naver.com>
2023-01-25 15:55:05 +0900

    MainRoastery 작업 보류

--- src/components/MainRoastery.jsx
+++ src/components/MainRoastery.jsx
@@ -1,7 +1,34 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
 
 export default function MainRoastery() {
+
+  const [roastery, setRoastery] = useState([]);
+
+  useEffect(() => {
+    axios({
+      method: "get",
+      url: "http://13.125.91.209:9002/roastery?sort=NAME",
+      // timeout: 5000,
+      responseType: "json",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json;charset=UTF-8;",
+      },
+    })
+      .then((res) => {
+        const data = JSON.stringify(res.data);
+        setRoastery(data);
+        console.log(data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, [])
+
   return (
-    <div>메인로스터리4곳</div>
+    <>
+    <div>메인로스터리 4곳</div>
+    </>
   )
 }
